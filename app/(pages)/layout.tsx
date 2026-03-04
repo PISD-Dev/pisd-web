95fa06aa6cba6bc6adba5498527168e2375eace6
Koniiro <81352867+Koniiro@users.noreply.github.com>
2026-03-05 02:46:55 +0800

    feat: About us
    setting up of About us page as well as layout for further pages

---
+++ app/(pages)/layout.tsx
@@ -0,0 +1,33 @@
+
+import { Geist, Geist_Mono } from "next/font/google";
+import "../globals.css";
+import NavBar from "@/components/navi_bar";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+      <div
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        <NavBar/>
+        {children}
+
+      </div>
+
+  );
+}
