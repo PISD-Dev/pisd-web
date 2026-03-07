b6c0360e17c9a95e0e9e6edbd3219a24916690da
Koniiro <81352867+Koniiro@users.noreply.github.com>
2026-03-07 16:45:34 +0800

    feat: initial about us page layout

--- app/(pages)/about_us/page.tsx
+++ app/(pages)/about_us/page.tsx
@@ -1,70 +1,143 @@
-
+import { lorem } from "@/components/project_description";
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import OrgChartWidget from "../../../components/orgchart_widget";
 
 export default function Home() {
   return (
-        <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-          <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-            <div>
-              About Us Dolor
-            </div>
-            <Image
-              className="dark:invert"
-              src="/next.svg"
-              alt="Next.js logo"
-              width={100}
-              height={20}
-              priority
-            />
-            <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-              <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-                To get started, edit the page.tsx file.
-              </h1>
-              <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-                Looking for a starting point or more instructions? Head over to{" "}
-                <a
-                  href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-                  className="font-medium text-zinc-950 dark:text-zinc-50"
-                >
-                  Templates
-                </a>{" "}
-                or the{" "}
-                <a
-                  href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-                  className="font-medium text-zinc-950 dark:text-zinc-50"
-                >
-                  Learning
-                </a>{" "}
-                center.
-              </p>
-            </div>
-            <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-              <a
-                className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-                href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <Image
-                  className="dark:invert"
-                  src="/vercel.svg"
-                  alt="Vercel logomark"
-                  width={16}
-                  height={16}
-                />
-                Deploy Now
-              </a>
-              <a
-                className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-                href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                Documentation
-              </a>
-            </div>
-    
-          </main>
+    <main className="flex flex-col min-h-screen   items-center  bg-white dark:bg-black sm:items-start">
+      <div className="bg-[#345995] w-full text-white py-3 text-center text-2xl">
+        Lorem Ipsum Dolor
+      </div>
+      <div className="grid grid-cols-[minmax(0,1fr)_40%] w-full gap-6 py-10 px-20">
+        <div className="flex flex-col gap-8 ">
+          <div className="text-7xl font-bold text-[#345995]">About Us</div>
+          <div className="flex flex-row gap-4 ">
+            <Button className="rounded-4xl font-bold bg-[#9B2242] hover:bg-[#741a32] text-md p-5">
+              Vision-Mission
+            </Button>
+            <Button className="rounded-4xl font-bold bg-[#F2CD00] text-black hover:bg-[#caac01] text-md p-5">
+              Organizational Chart
+            </Button>
+            <Button className="bg-[#47A23F] font-bold  rounded-4xl text-white hover:bg-[#33752d] text-md py-5 ">
+              History
+            </Button>
+          </div>
         </div>
+        <div className="bg-blue-700 w-full justify-center">
+          <Image
+            src="/img/flag.jpg"
+            alt="placeholder"
+            objectFit="cover"
+            width={200}
+            height={200}
+            loading="lazy"
+            className="px-3 shrink-0"
+          />
+        </div>
+      </div>
+      {/*Vision Mission*/}
+      <div className="bg-[#9B2242] text-white w-full grid grid-cols-3 py-12 px-10 gap-5">
+        <div className="flex flex-col gap-2">
+          <div className="font-black text-4xl">Vision</div>
+          <div>{lorem}</div>
+        </div>
+        <div className="flex flex-col gap-2">
+          <div className="font-black text-4xl">Mission</div>
+          <div>{lorem}</div>
+        </div>
+        <div className="flex flex-col gap-2">
+          <div className="font-black text-4xl">Mandate</div>
+          <div>{lorem}</div>
+        </div>
+      </div>
+
+      {/*Org Chart*/}
+      <div className="flex flex-col bg-[#F2CD00] text-white w-full  py-10 px-10 gap-5">
+        <div className="font-black text-4xl">Organization Chart</div>
+        <div className="flex flex-col gap-6">
+          <div className="flex  justify-around">
+            <OrgChartWidget
+              name="Juan Dela Cruz"
+              position="Standing Up"
+              img_link=""
+            />
+            <OrgChartWidget
+              name="Juan Dela Cruz"
+              position="Standing Up"
+              img_link=""
+            />
+            <OrgChartWidget
+              name="Juan Dela Cruz"
+              position="Standing Up"
+              img_link=""
+            />
+          </div>
+          <div className="flex justify-around">
+            <OrgChartWidget
+              name="Juan Dela Cruz"
+              position="Standing Up"
+              img_link=""
+            />
+            <OrgChartWidget
+              name="Juan Dela Cruz"
+              position="Standing Up"
+              img_link=""
+            />
+          </div>
+        </div>
+      </div>
+
+      {/*Vision Mission*/}
+      <div className="bg-[#47A23F] text-white w-full grid grid-cols-[minmax(0,1fr)_70%]  py-10 px-10 gap-20">
+        <div className="flex flex-col gap-2">
+          <div className="font-black text-4xl">History</div>
+          <div>{lorem}</div>
+        </div>
+        <div className="flex flex-row justify-around">
+          <div className="flex flex-col gap-2 items-center  border w-50">
+            <Image
+              src="/img/flag.jpg"
+              alt="flag"
+              width={200}
+              height={125}
+              className="aspect-square object-cover border"
+            />
+            <div className="text-center  line-clamp-5">{lorem}</div>
+          </div>
+          <div className="flex flex-col gap-2 items-center  border w-50">
+            <Image
+              src="/img/flag.jpg"
+              alt="flag"
+              width={200}
+              height={125}
+              className="aspect-square object-cover border"
+            />
+            <div className="text-center  line-clamp-5">{lorem}</div>
+          </div>
+          <div className="flex flex-col gap-2 items-center  border w-50">
+            <Image
+              src="/img/flag.jpg"
+              alt="flag"
+              width={200}
+              height={125}
+              className="aspect-square object-cover border"
+            />
+            <div className="text-center  line-clamp-5">{lorem}</div>
+          </div>
+          <div className="flex flex-col gap-2 items-center  border w-50">
+            <Image
+              src="/img/flag.jpg"
+              alt="flag"
+              width={200}
+              height={125}
+              className="aspect-square object-cover border"
+            />
+            <div className="text-center  line-clamp-5">{lorem}</div>
+          </div>
+          
+        </div>
+      </div>
+    </main>
   );
 }
